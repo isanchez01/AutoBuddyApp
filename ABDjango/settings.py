37434bdc9e73954e269f85f9be7b048f632c7980
Israel Sanchez <israel505114@gmail.com>
2023-03-16 21:49:18 -0500

    More Stuff
    More

--- ABDjango/settings.py
+++ ABDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!si$8(^am#bc#(y5kqp9ho4rak=63%^&mw#72b&$@d6*5g1!db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ABDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
